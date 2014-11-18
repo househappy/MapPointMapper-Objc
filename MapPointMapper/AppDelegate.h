1bf8cbc6120f40962deda62afd833d1ebbddd5b7
dmiedema <daniel@househappy.org>
2014-11-18 13:26:18 -0800

    Initial Commit

---
+++ MapPointMapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapPointMapper
+//
+//  Created by Daniel on 11/18/14.
+//  Copyright (c) 2014 Househappy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MapPointMapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapPointMapper
+//
+//  Created by Daniel on 11/18/14.
+//  Copyright (c) 2014 Househappy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
